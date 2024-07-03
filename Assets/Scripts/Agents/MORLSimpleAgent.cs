babdb4c2789dcf94fc79ab29248d6deb274c8c0f
Mart Kartasev <mart.kartasev@gmail.com>
2024-07-03 15:37:57 +0300

    Fixed goal pos. No normalization for flat envs. Refactored code. Added acceleration flat scene

--- Assets/Scripts/Agents/MORLSimpleAgent.cs
+++ Assets/Scripts/Agents/MORLSimpleAgent.cs
@@ -7,15 +7,15 @@ namespace Env5
     {
         public Transform player;
         public Transform goal;
-        
+
         public override void CollectObservations(VectorSensor sensor)
         {
             Vector3 playerPos = player.localPosition;
-            Vector3 playerPosObs = playerPos / envController.Width * 2f;
+            Vector3 playerPosObs = playerPos;
             sensor.AddObservation(playerPosObs);
-            
+
             Vector3 button2Pos = goal.localPosition;
-            Vector3 distanceTobutton2Obs = (button2Pos - playerPos) / envController.Width;
+            Vector3 distanceTobutton2Obs = (button2Pos - playerPos);
             sensor.AddObservation(distanceTobutton2Obs);
         }
     }
