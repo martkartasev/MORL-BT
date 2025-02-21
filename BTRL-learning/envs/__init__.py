b1d25b0c4da37c679d601b47e188cf983685a0b2
Your Name <you@example.com>
2025-02-21 07:51:32 +0100

    add unshapedSum task

--- BTRL-learning/envs/__init__.py
+++ BTRL-learning/envs/__init__.py
@@ -176,3 +176,20 @@ gym.envs.register(
         "max_ep_len": 200,
     },
 )
+gym.envs.register(
+    id="SimpleAccEnv-wide-withConveyer-unshapedSum-v0",
+    entry_point="envs:SimpleAccEnv",
+    max_episode_steps=200,
+    reward_threshold=0,
+    kwargs={
+        "task": "denseUnshapedSum",
+        "with_conveyer": True,
+        "x_max": 20,
+        "conveyer_x_min": 2,
+        "conveyer_x_max": 10,
+        "lava_x_min":  10,
+        "lava_x_max":  18,
+        "goal_x": 10,
+        "max_ep_len": 200,
+    },
+)
