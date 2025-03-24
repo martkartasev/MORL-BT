003423951d95c60c7e66a033cb3431830045c77d
Mart Kartasev <mart.kartasev@gmail.com>
2025-03-24 17:48:32 +0100

    Train feasibility

--- BTRL-learning/train_feasibility_abb.py
+++ BTRL-learning/train_feasibility_abb.py
@@ -35,7 +35,7 @@ def load_mlagents_buffer(load_dir, max_obs, feasibility_label, label_ratio=0.35)
         nr_files = len(replay_files)
         random.shuffle(replay_files)
 
-        #for i, file in enumerate(replay_files):
+        # for i, file in enumerate(replay_files):
         i = 1
 
         files_to_proccess = []
@@ -46,7 +46,7 @@ def load_mlagents_buffer(load_dir, max_obs, feasibility_label, label_ratio=0.35)
             filename = os.path.join(direc, file)
 
             files_to_proccess.append(filename)
-            i+=1
+            i += 1
             if len(files_to_proccess) >= parallel or len(replay_files) == 0:
                 async_results = [pool.apply_async(process_file, args=(feasibility_label, listFile, label_ratio)) for listFile in files_to_proccess]
                 results = [ar.get() for ar in async_results]
@@ -194,8 +194,6 @@ def train_model(
             save_model(device, exp_dir, model, states.shape[1], f"epoch_{epoch}")
 
     print("Done: training model")
-
-
 
     # if epoch % nuke_layer_every == 0 and epoch > 0:
 
@@ -372,11 +370,11 @@ def create_training_plots(exp_dir, train_loss_hist=None, lr_hist=None, pred_mean
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--rb_dirs", type=str, nargs="+", help="List of replay buffer directories to load data from", default=["C:/Users/Mart9/Workspace/ABB-Warehouse/results/move_ppo_rl/ABBMobile/"])
+    parser.add_argument("--rb_dirs", type=str, nargs="+", help="List of replay buffer directories to load data from", default=["C:/Users/Mart9/Workspace/ABB-Warehouse/results/move_ppo_penalty/ABBMobile/"])
     parser.add_argument("--buffer_size", type=int, help="Max size of replay buffer", default=10000000)
     parser.add_argument("--higher_prio_feasibility_estimator", type=str, help="Higher-prio feasibility estimator to load for recursive training", default="")
-    parser.add_argument("--exp_str", type=str, help="String to append to the experiment directory", default="safe&have128x64x64")
-    parser.add_argument("--feasibility_label", type=str, help="Which labelling function to use", default="safe&have")
+    parser.add_argument("--exp_str", type=str, help="String to append to the experiment directory", default="have128x64x64")
+    parser.add_argument("--feasibility_label", type=str, help="Which labelling function to use", default="have")
     parser.add_argument("--label_ratio", type=float, help="Minimum ratio between negative labelled data and all data. Between 0 and 1. 1 means all labels, 0 means no labels.", default=0.35)
     parser.add_argument("--epochs", type=int, help="Number of epochs to train the model", default=100)
     args = parser.parse_args()
