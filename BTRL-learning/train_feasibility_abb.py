17e685b8e5b4945f7c3f9ce66b36b1ac3214c932
Mart Kartasev <mart.kartasev@gmail.com>
2025-03-24 11:03:28 +0100

    Working

--- BTRL-learning/train_feasibility_abb.py
+++ BTRL-learning/train_feasibility_abb.py
@@ -223,7 +223,7 @@ def label_fun(state, feasibility_label):  # Now predicting that we are in the "G
     if feasibility_label == "safe&have":
         return state[0] > 0 and numpy.linalg.norm(state[16:19]) > 0.1
     if feasibility_label == "have&near":
-        return state[0] > 0 and numpy.linalg.norm(state[10:13]) < 0.65
+        return numpy.linalg.norm(state[16:19]) < 0.65 and state[0] > 0
     if feasibility_label == "have":
         return state[0] > 0
     if feasibility_label == "near":
@@ -372,12 +372,12 @@ def create_training_plots(exp_dir, train_loss_hist=None, lr_hist=None, pred_mean
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--rb_dirs", type=str, nargs="+", help="List of replay buffer directories to load data from", default=["C:/Users/Mart9/Workspace/ABB-Warehouse/results/move_ppo_rl/ABBMobile/"])
-    parser.add_argument("--buffer_size", type=int, help="Max size of replay buffer", default=10000000)
+    parser.add_argument("--rb_dirs", type=str, nargs="+", help="List of replay buffer directories to load data from", default=["C:/Users/Mart9/Workspace/ABB-Warehouse/results/place_ppo_rl/ABBMobile/"])
+    parser.add_argument("--buffer_size", type=int, help="Max size of replay buffer", default=15000000)
     parser.add_argument("--higher_prio_feasibility_estimator", type=str, help="Higher-prio feasibility estimator to load for recursive training", default="")
-    parser.add_argument("--exp_str", type=str, help="String to append to the experiment directory", default="safe128x64x64")
-    parser.add_argument("--feasibility_label", type=str, help="Which labelling function to use", default="safe")
-    parser.add_argument("--label_ratio", type=float, help="Minimum ratio between negative labelled data and all data. Between 0 and 1. 1 means all labels, 0 means no labels.", default=0.30)
+    parser.add_argument("--exp_str", type=str, help="String to append to the experiment directory", default="nearPlace128x64x64")
+    parser.add_argument("--feasibility_label", type=str, help="Which labelling function to use", default="have&near")
+    parser.add_argument("--label_ratio", type=float, help="Minimum ratio between negative labelled data and all data. Between 0 and 1. 1 means all labels, 0 means no labels.", default=0.4)
     parser.add_argument("--epochs", type=int, help="Number of epochs to train the model", default=50)
     args = parser.parse_args()
 
